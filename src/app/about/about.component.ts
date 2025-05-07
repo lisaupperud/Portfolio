92f95b27073c802b67d0ba6e98f784cebcfaf3fb
liluppis <lisa.upp@hotmail.com>
2025-05-07 15:49:18 +0200

    Add: structure and styling for AboutComponent, functions to expand articles -- needs more work

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-  logAboutComponent() {
-    console.log('AboutComponent loaded');
-  }
+  isWorkExpanded = false;
+  isProgrammingExpanded = false;
+  isInterestsExpanded = false;
+
+  shortText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...';
+  longText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies... hela texten här';
 }

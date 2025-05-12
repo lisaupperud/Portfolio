24c53f1e9d217656841886193378aee66994f51c
liluppis <lisa.upp@hotmail.com>
2025-05-12 21:07:50 +0200

    Add: design to ProjectsComponent

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,12 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  
 }
